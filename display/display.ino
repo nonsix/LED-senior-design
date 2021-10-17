a0221a7102658504f1850edab24fc625a774efde
Devon Schimming <xnomadsix@gmail.com>
2021-10-17 12:36:28 -0500

    Added more comments and starting working on packet handeling

--- display/display.ino
+++ display/display.ino
@@ -1,153 +1,104 @@
+// Author: Devon Schimming
+
 #include <SPI.h>
 #include <RH_RF95.h>
 
 RH_RF95 rf95(8, 3); // Adafruit Feather M0 with RFM95
 
-int led = 9;
 int delta_signal = 0; // timestamp from last signal in milliseconds
 
+// Each avaliable state
 enum STATE
 {
-    WAITING = 0,
+    IDLE = 0,
     WARNING = 1,
-    EMERGENCY = 2,
-    IDLE = 3,
 };
 
-enum SIGNAL
-{
-    PIR = 0,
-    BREAK_BEAM = 1
-};
+// default state
+STATE current_state = IDLE;
 
-STATE current_state = WAITING;
-
+// Takes a signal code and sets the display state accordingly.
 STATE handel_signal(SIGNAL signal)
 {
     switch (current_state)
     {
-    case WAITING:
-        switch (signal)
-        {
-        case PIR:
-            current_state = WARNING;
-            break;
-        case BREAK_BEAM:
-            current_state = WARNING;
-            break;
-        }
+    case IDLE:
+        current_state = WARNING;
         break;
     case WARNING:
-        switch (signal)
-        {
-        case PIR:
-            current_state = WARNING;
-            break;
-        case BREAK_BEAM:
-            current_state = EMERGENCY;
-            break;
-        }
-        break;
-    case EMERGENCY:
-        switch (signal)
-        {
-        case PIR:
-            current_state = EMERGENCY;
-            break;
-        case BREAK_BEAM:
-            current_state = EMERGENCY;
-            break;
-        }
+        current_state = WARNING;
         break;
     }
+    delta_signal = millis();
 }
 
-bool state_timeout(int delta_time)
+// delta_time:  Time sense last signal received
+// timeout:     Maximum time a state is allowed to display
+bool state_timeout(int delta_time, int timeout)
 {
-    int timeout = 60000; // 1 min in milliseconds
-    return delta_time > timeout;
+    return (millis() - delta_time) > timeout;
 }
 
+// Sets the inital values for the display
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600);
-    current_state = WARNING;
+    current_state = IDLE;
     while (!Serial)
         ; // Wait for serial port to be available
     if (!rf95.init())
         Serial.println("init failed");
 }
 
-void update()
-{
-    // move sensor handel here
-}
-
+// state:   The state the display is going to represent
 void handel_state(STATE state)
 {
-    // based on current_state display the correct led output
     switch (state)
     {
-    case WAITING:
-        Serial.print("waiting, ");
+    case IDLE:
+        Serial.print("idle. ");
         digitalWrite(LED_BUILTIN, HIGH);
-        delay(1000);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(1000);
         break;
 
     case WARNING:
         Serial.print("warning, ");
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(500);
         digitalWrite(LED_BUILTIN, LOW);
         delay(500);
-
-        break;
-
-    case EMERGENCY:
-        Serial.print("emergency, ");
         digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(100);
-        break;
-
-    case IDLE:
-        Serial.print("idle.");
-        digitalWrite(LED_BUILTIN, LOW);
+        delay(500);
         break;
     }
 }
 
 void loop()
 {
-    // if (rf95.available())
-    // {
-    //     // uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    //     // uint8_t len = sizeof(buf);
-    //     // if (rf95.recv(buf, &len))
-    //     // {
-    //     //     // digitalWrite(led, HIGH);
-    //     //     // Serial.print("got singal: ");
-    //     //     // Serial.println((int *)buf);
-    //     //     // handel_signal((int *)buf)
-    //     //     // if (!state_timeout(delta_signal))
-    //     //     // {
-    //     //     //     delta_signal = milli() - delta_signal; // calcs how much time passed sense last signal
-    //     //     // } else {
-    //     //     //     current_state = WAITING;
-    //     //     //     delta_signal = 0;
-    //     //     // }
-    //     //     update();
-    //     //     display(current_state);
-    //     // }
-    //     // else
-    //     // {
-    //     //     Serial.println("recv failed");
-    //     // }
-    // }
-    update();
+    if (rf95.available())
+    {
+        uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+        uint8_t len = sizeof(buf);
+        if (rf95.recv(buf, &len))
+        {
+            Serial.print("Signal recvied. ");
+            Serial.println((int *)buf);
+            // TODO: add code to change states from signal
+        }
+        else
+        {
+            Serial.println("Signal failed. ");
+        }
+    }
+
+    const int millis_timeout = 2500;
+    if (state_timeout(delta_signal, millis_timeout))
+    {
+        // cycles through each state
+        current_state = STATE((current_state + WARNING) % 2);
+
+        // reset timestamp to current time
+        delta_signal = millis();
+    }
+
+    // sets display to the correct mode based on current_state
     handel_state(current_state);
 }
